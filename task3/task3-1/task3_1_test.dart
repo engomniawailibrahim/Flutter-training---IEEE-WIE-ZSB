6fd658091eafa8138ec42c879f64989e283ea7ea
engomniawailibrahim <128514406+engomniawailibrahim@users.noreply.github.com>
2023-05-14 02:48:52 +0300

    Add files via upload

---
+++ task3/task3-1/task3_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:task3_1/task3_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ task3/task3-2/task3_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:task3_2/task3_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
